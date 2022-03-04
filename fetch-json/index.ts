7751c43bd49031a4f19b9d6c4ebf87e547a232b6
khaled hossain <md.khaled.eee@gmail.com>
2022-03-04 08:14:53 +0600

    Add interface for network data

---
+++ fetch-json/index.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  logTodo(todo);
+});
+
+function logTodo(todo: Todo) {
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  console.log(`${ID}. ${title} - finished? - ${completed}`);
+}
